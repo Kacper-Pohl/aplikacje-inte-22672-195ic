4a2fb4e51d517cc36e5905c001f9e4d08b1a4aee
Kacper-Pohl <kacper.pohl.99@gmail.com>
2021-11-19 23:04:32 +0100

    Fix forms.py, oraz requirements.txt

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Post
 
@@ -8,8 +10,10 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
+
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
