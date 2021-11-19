a457a04b8ea14368e3cc2c435891ecd02719e59c
Kacper-Pohl <kacper.pohl.99@gmail.com>
2021-11-19 22:53:26 +0100

    Test lab2 heroku

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 from .models import Post
 
-class PostForm(forms.ModelForm):
 
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
