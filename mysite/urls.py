19e623b3c89aea9fe6f6a08098d0c6d241353cf5
Kacper-Pohl <kacper.pohl.99@gmail.com>
2021-11-20 01:00:33 +0100

    Poprawki w urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path(
         'change_password/',
@@ -36,6 +37,4 @@ urlpatterns = [
     ),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('', include('blog.urls')),
-
 ]
