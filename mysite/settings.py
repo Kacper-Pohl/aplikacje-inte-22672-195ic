9b5a6a5baa60fe7cbcd8c2e58b99e01428920e50
Kacper-Pohl <kacper.pohl.99@gmail.com>
2021-11-20 01:57:44 +0100

    fix settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-LOGIN_REDIRECT_URL = '/'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
