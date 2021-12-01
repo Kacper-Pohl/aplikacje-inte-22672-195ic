de3fd99b7bf5c9306943f96046917a0b77d552b1
Kacper-Pohl <kacper.pohl.99@gmail.com>
2021-12-01 20:49:27 +0100

    Lab 3 - fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,7 +151,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from blog import views
-from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
